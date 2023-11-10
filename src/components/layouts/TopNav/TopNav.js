616411e886dda236492e8b703237c708bee01de9
Josué Yovani <josue.yovani@outlook.com>
2023-11-09 23:31:15 -0600

    Fix - Change background color button header

--- src/components/layouts/TopNav/TopNav.js
+++ src/components/layouts/TopNav/TopNav.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-export const TopNav = () => {
+export const TopNav = (props) => {
+  const { scrolled } = props;
+
   return (
     <div className="top-nav">
       <span>Our story</span>
       <span>membership</span>
       <span>Write</span>
       <span>Sign In</span>
-      <button className="btn">Get started</button>
+      <button className={`${scrolled ? "btn-secondary" : "btn"}`}>
+        Get started
+      </button>
     </div>
   );
 };
